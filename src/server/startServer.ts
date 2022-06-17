c885fde977f67bfad1d7410d0e8e05214ab87c82
anticher <anticherq@gmail.com>
2022-06-18 02:18:58 +0300

    build: add webpack and start scripts

--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -2,32 +2,34 @@ import * as http from 'http'
 import { handleGet } from '../get/handleGet'
 import { handlePost } from '../post/handlePost'
 
-const host = 'localhost'
-const port = 8000
+export const startServer = () => {
+    const host = 'localhost'
+    const port = 8000
 
-const requestListener:
-    http.RequestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
-        if (req.url?.startsWith('/api/')) {
-            switch (true) {
-                case req.method === 'GET':
-                    handleGet(req, res)
-                    break
-                case req.method === 'POST':
-                    handlePost(req, res)
-                    break
-                default:
-                    res.writeHead(404)
-                    res.end(JSON.stringify({ error: 'wrong path' }))
-                    break
+    const requestListener:
+        http.RequestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
+            if (req.url?.startsWith('/api/')) {
+                switch (true) {
+                    case req.method === 'GET':
+                        handleGet(req, res)
+                        break
+                    case req.method === 'POST':
+                        handlePost(req, res)
+                        break
+                    default:
+                        res.writeHead(404)
+                        res.end(JSON.stringify({ error: 'wrong path' }))
+                        break
+                }
+            } else {
+                res.writeHead(404)
+                res.end(JSON.stringify({ error: 'wrong path' }))
             }
-        } else {
-            res.writeHead(404)
-            res.end(JSON.stringify({ error: 'wrong path' }))
         }
-    }
 
-const server = http.createServer(requestListener)
+    const server = http.createServer(requestListener)
 
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)
-})
+    server.listen(port, host, () => {
+        console.log(`Server is running on http://${host}:${port}`)
+    })
+}
